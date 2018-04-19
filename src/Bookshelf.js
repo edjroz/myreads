230eeb81ea8e9bd321f7089951766c2f35f0745e
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2018-04-19 11:56:44 -0400

    Initial Commit

---
+++ src/Bookshelf.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class Bookshelf extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+export default Bookshelf;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// TODO: add react router config
+ReactDOM.render(<App />, document.getElementById('root'));
