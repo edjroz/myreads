2273f3d610bac9e95ac80f1194de69085b1b281f
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2018-04-19 22:22:11 -0400

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import Bookshelf from './Bookshelf';
+import Search from './Search';
 import './App.css';
 
+// TODO: get all books from server
 class BooksApp extends React.Component {
   state = {
     /**
@@ -11,62 +14,91 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    books: [
+      {
+        url:
+          'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api',
+        author: 'Orson Scott Card',
+        title: "Ender's Game",
+        shelf: 'Currently Reading',
+      },
+      {
+        url:
+          'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
+        author: 'Harper Lee',
+        title: 'To Kill a Mockingbird',
+        shelf: 'Currently Reading',
+      },
+      {
+        url:
+          'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api',
+        author: 'J.K Rowling',
+        title: "Harry potter and the Sorcerer's Stone",
+        shelf: 'Want to Read',
+      },
+      {
+        url:
+          'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api',
+
+        author: 'David McCullough',
+        title: '1776',
+        shelf: 'Want to Read',
+      },
+      {
+        url:
+          'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api',
+        author: 'J.R.R Tolkien',
+        title: 'The Hobbit',
+        shelf: 'Read',
+      },
+      {
+        url:
+          'http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api',
+        author: "Oh, the Places You'll go!",
+        title: 'Seuss',
+        shelf: 'Read',
+      },
+    ],
     showSearchPage: false,
   };
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <Bookshelf />
+        <Route
+          path="/search"
+          render={() => {
+            return <Search />;
+          }}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
                 </div>
-                <div className="bookshelf">
-                  <Bookshelf />
+                <div className="list-books-content">
+                  <div>
+                    <Bookshelf
+                      title="Currently Reading"
+                      books={this.state.books}
+                    />
+                    <Bookshelf title="Want To Read" books={this.state.books} />
+                    <Bookshelf title="Read" books={this.state.books} />
+                  </div>
                 </div>
-                <div className="bookshelf">
-                  <Bookshelf />
+                <div className="open-search">
+                  <Link className="open-search" to="/search">
+                    Add a book
+                  </Link>
                 </div>
               </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </a>
-            </div>
-          </div>
-        )}
+            );
+          }}
+        />
       </div>
     );
   }
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Book from './Book';
 
 class Bookshelf extends React.Component {
+  state = {
+    books: this.props.books.filter((book) => {
+      console.log(this.props);
+      return book.shelf.toLowerCase() === this.props.title.toLowerCase();
+    }),
+  };
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.state.books.map((book) => {
+              return (
+                <li key={book.title}>
+                  <Book
+                    url={book.url}
+                    author={book.author}
+                    title={book.title}
+                  />
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    );
   }
 }
 export default Bookshelf;
